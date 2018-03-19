2882a481dc546b7fa9f67361ea367ebc599c6dc4
Connor <wscofield@elon.edu>
2018-03-19 10:36:45 -0400

    Working Code, finishing CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,7 @@ var corrLetts = document.getElementById("userEntered");
 var winDisp = document.getElementById("winDisp");
 var lossDisp = document.getElementById("lossDisp");
 var boardDisp = document.getElementById("boardDisp");
+var charImg = document.getElementById("TBHImg");
 
 
 //Assigned and Initialized Variables
@@ -16,7 +17,7 @@ var wins = 0;
 //losses
 var losses = 0;
 //List of Possible Words
-var allWords = ["BIRMINGHAM", "TOMMY", "ARTHUR", "POLLY", "MICHAEL", "GRACE", "PEAKYBLINDERS", "GYPSIES", "ALFIE", "THEDERBY", "WINSTONCHURCHILL"]
+var allWords = ["TOMMY", "ARTHUR", "POLLY", "MICHAEL", "GRACE", "ALFIE", "CAMPBELL"]
 //Current gameState
 var gameState; //reset -- gameOn -- gameOver -- reset
 var lettGuessArr = [];
@@ -64,12 +65,13 @@ document.onkeydown = function (event) {
 
 function reset() {
     gameState = 'reset';
-    startText.textContent = "Press Enter to Begin!";
+    startText.textContent = "Press Enter to start the game";
     numAttempts.textContent = "";
     lettGuessedDisp.textContent = "";
     boardDisp.innerHTML = "&#9830;&#9830;&#9830;&#9830;&#9830;&#9830;&#9830;&#9830;&#9830;&#9830;";
     lettGuessedDisp.textContent = "";
     // img of peaky blinders character
+    charImg.src = "assets/images/PBCover.jpg"
 
 }
 
@@ -93,71 +95,101 @@ function init() {
 }
 
 
-function update(letter) {
-    //check input to see if right
-    var correct = false;
-    //if correct set to correct
-    for (var i = 0; i < unknownWord.length; i++) {
-        if (letter == unknownWord[i]) {
-            correct = true;
+    function update(letter) {
+        //check input to see if right
+        var correct = false;
+        //if correct set to correct
+        for (var i = 0; i < unknownWord.length; i++) {
+            if (letter == unknownWord[i]) {
+                correct = true;
+            }
+
+        }
+
+        //then update the letters guessed array with .push (if incorrect)
+        //and the display.text
+
+        lettGuessArr.push(letter);
+        var letGuess = "";
+        for (var i = 0; i < lettGuessArr.length; i++) {
+            letGuess += lettGuessArr[i];
+        }
+        lettGuessedDisp.textContent = letGuess;
+
+
+        //Update attempts and the board space
+        if (correct) {
+            boardDisp.textContent = updateBoard(unknownWord, lettGuessArr);
+
+        }
+        else {
+            numGuesses -= 1;
+            numAttempts.textContent = numGuesses;
+
+        }
+
+        //check game loss conditions
+        if (numGuesses < 1) {
+            gameLose();
+        }
+
+        //check game win conditions
+        var win = true;
+        var x = 0;
+        while (x < boardDisp.textContent.length && win == true) {
+            if (boardDisp.textContent[x] == "_") {
+                win = false;
+            }
+            x++;
+        }
+        if (win) {
+            gameWin();
         }
 
     }
 
-    //then update the letters guessed array with .push (if incorrect)
-    //and the display.text
 
-    lettGuessArr.push(letter);
-    var letGuess = "";
-    for (var i = 0; i < lettGuessArr.length; i++) {
-        letGuess += lettGuessArr[i];
-    }
-    lettGuessedDisp.textContent = letGuess;
+    function gameLose() {
+        //change state
+        gameState = "gameOver";
 
+        //change message to user text
+        startText.innerHTML = "You did not guess correctly!"+ unknownWord + " is to be hung! Press space to play again.";
 
-    //Update attempts and the board space
-    if (correct) {
-        boardDisp.textContent = updateBoard(unknownWord, lettGuessArr);
+        //update # of losses
+        losses += 1;
+        lossDisp.textContent = losses;
 
-    }
-    else {
-        numGuesses -= 1;
-        numAttempts.textContent = numGuesses;
+        //change img being displayed depending on unknownword
+        switch (unknownWord) {
+            case ('CAMPBELL'):
+                charImg.src = 'assets/images/Campbell.jpg';
+                break;
 
-    }
+            case ('ARTHUR'):
+                charImg.src = 'assets/images/arthur.jpg';
+                break;
 
-    //check game loss conditions
-    if (numGuesses < 1) {
-        gameLose();
-    }
+            case ('ALFIE'):
+                charImg.src = 'assets/images/alfie.jpg';
+                break;
 
-    //check game win conditions
-    var win = true;
-    var x = 0;
-    while (x < boardDisp.textContent.length && win == true) {
-        if (boardDisp.textContent[x] == "_") {
-            win = false;
+            case ('TOMMY'):
+                charImg.src = 'assets/images/tommy.png';
+                break;
+
+            case ('GRACE'):
+                charImg.src = 'assets/images/grace.jpg';
+                break;
+
+            case ('MICHAEL'):
+                charImg.src = 'assets/images/michael.jpg';
+                break;
+
+            case ('POLLY'):
+                charImg.src = 'assets/images/inspectorCampbell.jpg';
+                break;
         }
-        x++;
-    }
-    if (win) {
-        gameWin();
-    }
-
-}
-
-
-function gameLose() {
-    //change state
-    gameState = "gameOver";
-
-    //change message to user text
-    startText.innerHTML = "You did not guess correctly! You lose!";
-
-    //update # of losses
-    losses += 1;
-    lossDisp.textContent = losses;
-
 
 }
 
@@ -166,13 +198,43 @@ function gameWin() {
     gameState = "gameOver";
 
     //change the msg to the user
-    startText.innerHTML = "You guessed correct! You obviously know your Peaky Blinders. Press SPACE to play again!";
+    startText.innerHTML = "You guessed correct! Looks like " + unknownWord +    " won't be hung. Press SPACE to play again!";
 
 
     //update # of wins
     wins += 1;
     winDisp.textContent = wins;
 
+    //change img being displayed depending on unknownword
+    switch (unknownWord) {
+        case ('CAMPBELL'):
+            charImg.src = 'assets/images/Campbell.jpg';
+            break;
+
+        case ('ARTHUR'):
+            charImg.src = 'assets/images/arthur.jpg';
+            break;
+
+        case ('ALFIE'):
+            charImg.src = 'assets/images/alfie.jpg';
+            break;
+
+        case ('TOMMY'):
+            charImg.src = 'assets/images/tommy.png';
+            break;
+
+        case ('GRACE'):
+            charImg.src = 'assets/images/grace.jpg';
+            break;
+
+        case ('MICHAEL'):
+            charImg.src = 'assets/images/michael.jpg';
+            break;
+
+        case ('POLLY'):
+           charImg.src = 'assets/images/polly.jpg';
+            break;
+    }
 
 
 }
